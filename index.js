8701d4fc15f9b373f13d6ae04d3490a88ba32609
Dilip D <110930713+dilip-d@users.noreply.github.com>
2023-03-01 11:02:26 +0530

    updated to deploy

--- index.js
+++ index.js
@@ -11,28 +11,28 @@ import messageRouter from './routes/messagesRoute.js';
 import dotenv from 'dotenv';
 import fileUpload from 'express-fileupload';
 import { Server } from 'socket.io';
-
-dotenv.config();
+// import { sockets } from './sockets/socket.js';
 
 const app = express();
-
-app.use(morgan('dev'));
-app.use(express.json({ extended: true, parameterLimit: 1000000, limit: '10000kb' }))
-app.use(express.urlencoded({ extended: true, parameterLimit: 1000000, limit: '10000kb' }))
-
-app.use(fileUpload({
-  useTempFiles: true
-}))
+dotenv.config();
 
 const corsOptions = {
   origin: 'https://bfitpro.netlify.app',
   credentials: true,
+  optionSuccessStatus: 200,
 };
+
+app.use(morgan('dev'));
+app.use(express.json({ extended: true, parameterLimit: 1000000, limit: '10000kb' }))
+app.use(express.urlencoded({ extended: true, parameterLimit: 1000000, limit: '10000kb' }))
+app.use(fileUpload({
+  useTempFiles: true
+}))
 app.use(cors(corsOptions));
 
 app.use('/', userRouter);
-app.use('/admin', adminRouter)
-app.use('/trainer', trainerRouter)
+app.use('/admin', adminRouter);
+app.use('/trainer', trainerRouter);
 app.use('/conversations', conversationRouter);
 app.use('/messages', messageRouter);
 
@@ -51,82 +51,82 @@ try {
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["https://bfitpro.netlify.app"],
+    origin: "https://bfitpro.netlify.app",
     credentials: true,
   }
 });
 
-// to create socketId according t email incoming
-const emailToSocketMapping = new Map();
-const socketToEmailMapping = new Map();
+// io.on('connection', sockets);
+
+// to create socketId according to email incoming
+// const emailToSocketMapping = new Map();
+// const socketToEmailMapping = new Map();
 
 let users = [];
 
 const adduser = (userId, socketId) => {
-  !users.some(user => user.userId === userId) &&
+  !users.some((user) => user.userId === userId) &&
     users.push({ userId, socketId })
 }
 
 const getUser = (userId) => {
-  console.log("users",users);
-  return users.find(user => user.userId === userId)
+  return users.find((user) => user.userId === userId)
 }
 
 const removeUser = (socketId) => {
-  users = users.filter((user => user.socketId !== socketId))
+  users = users.filter((user) => user.socketId !== socketId)
 }
 
-
 io.on("connection", (socket) => {
-  console.log("New Connection");
+  console.log("Socket Connected");
 
   // take userid and socketid from the user
-  socket.on("addUser", userId => {
+  socket.on("addUser", (userId) => {
     adduser(userId, socket.id)
-    io.emit("getUsers", users)
-  })
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-    removeUser(socket.id)
     io.emit("getUsers", users)
   })
 
   //send and get message
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
-    console.log(111, user);
     io.to(user?.socketId).emit("getMessage", {
       senderId,
       text,
     })
   });
 
-  socket.on("join-room", (data) => {
-    const { roomId, emailId } = data;
-    console.log("User", emailId, "Joined Room", roomId);
-    emailToSocketMapping.set(emailId, socket.id);
-    socketToEmailMapping.set(socket.id, emailId);
-    socket.join(roomId);
-    socket.emit("joined-room", { roomId });
-    console.log('joineddddd');
-    socket.broadcast.to(roomId).emit("user-joined", { emailId });
-  });
-
-  socket.on("call-user", (data) => {
-    const { emailId, offer } = data;
-    const fromEmail = socketToEmailMapping.get(socket.id);
-    const socketId = emailToSocketMapping.get(emailId);
-    socket.to(socketId).emit('incoming-call', { from: fromEmail, offer });
-  });
-
-  socket.on("call-accepted", (data) => {
-    const { emailId, ans } = data;
-    const socketId = emailToSocketMapping.get(emailId);
-    socket.to(socketId).emit("call-accepted", { ans })
-  });
-
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    removeUser(socket.id)
+    io.emit("getUsers", users)
+  })
 });
+
+  //   socket.on("join-room", (data) => {
+  //     const { userid } = data;
+  //     console.log("User", userid, "Joined Room");
+  //     emailToSocketMapping.set(userid, socket.id);
+  //     socketToEmailMapping.set(socket.id, userid);
+  //     socket.join(userid);
+  //     socket.emit("joined-room", { userid });
+  //     console.log('joineddddd');
+  //     socket.broadcast.to(userid).emit("user-joined", { userid });
+  //   });
+
+  //   socket.on("call-user", (data) => {
+  //     const { emailId, offer } = data;
+  //     const fromEmail = socketToEmailMapping.get(socket.id);
+  //     const socketId = emailToSocketMapping.get(emailId);
+  //     socket.to(socketId).emit('incoming-call', { from: fromEmail, offer });
+  //   });
+
+  //   socket.on("call-accepted", (data) => {
+  //     const { emailId, ans } = data;
+  //     const socketId = emailToSocketMapping.get(emailId);
+  //     socket.to(socketId).emit("call-accepted", { ans })
+  //   });
+
+
 
 //connecting with an event & event listener
 // io.on('connection', (socket) => {
@@ -158,4 +158,3 @@ io.on("connection", (socket) => {
 //   socket.on('answerCall', (data) => {
 //     io.to(data.to).emit('callAccepted', data.signal);
 //   })
-// })
